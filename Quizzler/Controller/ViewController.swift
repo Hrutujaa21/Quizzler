837158f2cd16ad54b96e7b1561dbf8d20cbc392c
Hrutujaa21 <kamthe.hrutuja@gmail.com>
2025-01-22 21:46:38 +0530

    Modified code to multiple choice questions

--- Quizzler/Controller/ViewController.swift
+++ Quizzler/Controller/ViewController.swift
@@ -11,9 +11,14 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var questionLabel: UILabel!
     @IBOutlet weak var progressBar: UIProgressView!
-    @IBOutlet weak var trueButton: UIButton!
-    @IBOutlet weak var falseButton: UIButton!
+//    @IBOutlet weak var trueButton: UIButton!
+//    @IBOutlet weak var falseButton: UIButton!
     @IBOutlet weak var scoreLabel: UILabel!
+    
+    //Added another button for multiple choice questions.
+    @IBOutlet weak var choice1: UIButton!
+    @IBOutlet weak var choice2: UIButton!
+    @IBOutlet weak var choice3: UIButton!
     
     var quizBrain = QuizBrain()
 
@@ -39,11 +44,20 @@ class ViewController: UIViewController {
 
     @objc func updateUI() {
         questionLabel.text = quizBrain.getQuestionText()
+        
         progressBar.progress = quizBrain.getProgress()
         scoreLabel.text = "Score: \(quizBrain.getScore())"
         
-        trueButton.backgroundColor = .clear
-        falseButton.backgroundColor = .clear
+        let answerChoices = quizBrain.getAnswers()
+        choice1.setTitle(answerChoices[0], for: .normal)
+        choice2.setTitle(answerChoices[1], for: .normal)
+        choice3.setTitle(answerChoices[2], for: .normal)
+        
+//        trueButton.backgroundColor = .clear
+//        falseButton.backgroundColor = .clear
+        choice1.backgroundColor = .clear
+        choice2.backgroundColor = .clear
+        choice3.backgroundColor = .clear
     }
 }
 
